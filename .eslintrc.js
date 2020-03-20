57044934571f52e2665d31d63326c01bf88a6a85
Niklas Koep <niklas.koep@gmail.com>
2020-03-20 09:13:39 +0100

    package.json: add 'lint' command
    Signed-off-by: Niklas Koep <niklas.koep@gmail.com>

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "webextensions": true,
+    "es6": true
+  },
+  "extends": "eslint:recommended",
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly"
+  },
+  "parserOptions": {
+    "ecmaVersion": 2019,
+    "sourceType": "module"
+  },
+  "rules": {
+  }
+};
